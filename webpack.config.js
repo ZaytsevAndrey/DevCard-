8c91aac3eff6f7875477b24b88d3292724ee7905
Andriy Zaytsev <adryathic@gmail.com>
2025-02-03 18:48:43 +0100

    add files to build source

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,50 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|ico|JPG)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
+
       {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: '' }, // Копіює всі файли з public до dist
-      ],
+      patterns: [{ from: "public", to: "" }],
     }),
   ],
+
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true,
+    static: path.resolve(__dirname, "public"),
+    port: 8000,
     open: true,
   },
-  mode: 'production', // Змінюйте на 'production' для білду
 };
