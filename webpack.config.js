aa22c9e389d10c14e96f148f24ceba173a2d59c5
Andriy Zaytsev <adryathic@gmail.com>
2025-02-03 18:42:50 +0100

    add files to build source

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: 'babel-loader',
       },
       {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        test: /\.(png|jpg|jpeg|gif|svg|ico|JPG)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      favicon: "./public/favicon.ico",
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' }, // Копіює всі файли з public до dist
+      ],
     }),
   ],
   devServer: {
-    static: path.resolve(__dirname, "public"),
-    port: 8000,
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true,
     open: true,
   },
+  mode: 'production', // Змінюйте на 'production' для білду
 };
